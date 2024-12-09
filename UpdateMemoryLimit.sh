68ca3a2cd9fe96eb729e9306252e3822a9506b2c
SebastienLemoine <100276459+SebLOclock@users.noreply.github.com>
2024-12-09 08:46:23 +0100

    Update UpdateMemoryLimit.sh

--- UpdateMemoryLimit.sh
+++ UpdateMemoryLimit.sh
@@ -1,2 +1,5 @@
-echo "define('WP_MEMORY_LIMIT', '512M');" >> /var/www/html/wp-config.php
+echo "
+if( ! define('WP_MEMORY_LIMIT'){
+  define('WP_MEMORY_LIMIT', '512M');
+}" >> ./wordpress/wp-config.php
 echo "La limite mémoire de Wordpress à été redéfinie"
