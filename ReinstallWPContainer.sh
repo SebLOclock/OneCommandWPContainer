5e7a05a5f01bb15ebfc116c0d0581abd80263629
SebastienLemoine <100276459+SebLOclock@users.noreply.github.com>
2025-03-19 14:16:25 +0100

    Update ReinstallWPContainer.sh

--- ReinstallWPContainer.sh
+++ ReinstallWPContainer.sh
@@ -1,9 +1,9 @@
 # Suppression des conteneurs:
-cd /home/$(whoami)/wordpress
+cd /home/student/wordpress
 docker compose down
 # Suppression des fichiers:
-rm -rf /home/$(whoami)/OneCommandWPContainer/wordpress
-rm -rf /home/$(whoami)/OneCommandWPContainer/uploads.ini
+rm -rf /home/student/OneCommandWPContainer/wordpress
+rm -rf /home/student/OneCommandWPContainer/uploads.ini
 
 # Docker rm
 docker rm $(docker ps -a -q) -f
