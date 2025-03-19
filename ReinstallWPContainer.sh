ec453e61675893ef686428bd02461909f0c9b795
SebLOclock <sebastien.lemoine@oclock.io>
2025-03-19 13:09:33 +0100

    reinstall

---
+++ ReinstallWPContainer.sh
@@ -0,0 +1,11 @@
+# Suppression des conteneurs:
+cd /home/$(whoami)/wordpress
+docker compose down
+# Suppression des fichiers:
+rm -rf /home/$(whoami)/OneCommandWPContainer/wordpress
+rm -rf /home/$(whoami)/OneCommandWPContainer/uploads.ini
+
+# Docker rm
+docker rm $(docker ps -a -q) -f
+
+sh /home/$(whoami)/OneCommandWPContainer/InstallWPContainer.sh
